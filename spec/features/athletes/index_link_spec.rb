17dd3191d3d9f981384c739b3f3f9930a9658212
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-02-10 08:12:19 -0700

    Complete story 22 test

---
+++ spec/features/athletes/index_link_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor" do
+  describe "When I visit any page on the site, there is a link to take me back to the Athletes Index'" do
+    it "It takes takes you back to athletes index" do
+      team1 = Team.create(city: "Boston",
+                          name: "Celtics",
+                          playoff_picture: true,
+                          championships: 19,
+                          id: 1)
+      athlete1 = Athlete.create(name: "Paul Pierce",
+                          age: 42,
+                          injury_list: true,
+                          team_id: 1,
+                          id: 1)
+
+      visit "/teams"
+      expect(page).to have_link('Athlete Index')
+      click_link('Athlete Index')
+      expect(page).to have_current_path('/athletes')
+
+      visit "/teams/new"
+      expect(page).to have_link('Athlete Index')
+      click_link('Athlete Index')
+      expect(page).to have_current_path('/athletes')
+
+      visit "/teams/#{team1.id}"
+      expect(page).to have_link('Athlete Index')
+      click_link('Athlete Index')
+      expect(page).to have_current_path('/athletes')
+
+      visit "/teams/#{team1.id}/edit"
+      expect(page).to have_link('Athlete Index')
+      click_link('Athlete Index')
+      expect(page).to have_current_path('/athletes')
+
+      visit "/teams/#{team1.id}/athletes"
+      expect(page).to have_link('Athlete Index')
+      click_link('Athlete Index')
+      expect(page).to have_current_path('/athletes')
+
+      visit "/athletes/#{athlete1.id}"
+      expect(page).to have_link('Athlete Index')
+      click_link('Athlete Index')
+      expect(page).to have_current_path('/athletes')
+
+      visit "/athletes/#{athlete1.id}/edit"
+      expect(page).to have_link('Athlete Index')
+      click_link('Athlete Index')
+      expect(page).to have_current_path('/athletes')
+
+      visit "/teams/#{team1.id}/athletes/new"
+      expect(page).to have_link('Athlete Index')
+      click_link('Athlete Index')
+      expect(page).to have_current_path('/athletes')
+    end
+  end
+end
